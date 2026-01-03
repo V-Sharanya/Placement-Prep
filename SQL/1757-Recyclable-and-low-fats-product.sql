74b1f197fc520467db0ae74e45cbbe630ada6227
V-Sharanya <sharanyav.146@gmail.com>
2026-01-03 20:48:11 +0530

    Day 1 of SQL Practice

---
+++ SQL/1757-Recyclable-and-low-fats-product.sql
@@ -0,0 +1,6 @@
+SELECT product_id from Products
+WHERE low_fats = "Y" and recyclable = "Y";
+
+
+--Notes
+--An ENUM (short for enumeration) in SQL is a string data type used to restrict the values in a column to a predefined, static list of permitted options. 
---
+++ SQL/584-Find-Customer-Referee.sql
@@ -0,0 +1,2 @@
+SELECT name from Customer
+WHERE referee_id !=2 or referee_id IS NULL;
